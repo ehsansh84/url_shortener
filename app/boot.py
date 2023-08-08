15ae962f7f5ae59a34ddf012f98311f4e7326ef9
ehsan <ehsan.shirzadi@gmail.com>
2023-08-08 23:54:04 +0330

    added: list and query

--- app/boot.py
+++ app/boot.py
@@ -5,9 +5,29 @@ sys.path.append(root_dir)
 from fastapi import FastAPI
 import uvicorn
 from app.api.url import router as url_router
-
+from app.db_models.url import Url
 app = FastAPI()
 app.include_router(router=url_router)
+
+
+@app.get("/{_id}", response_description=f"Redirect to original url")
+async def get_one(_id):
+    from app.core.base62 import Base62
+    if _id != 'favicon.ico':
+        order = Base62.decode_base_62(_id)
+        obj = Url()
+        result = obj.list({'order': order})
+        print(result[0])
+        print(len(result))
+    #
+    # if obj.is_loaded():
+    #     return obj.to_json()
+    # else:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND,
+    #         detail=f'{module_text} with id {_id} not found!',
+    #     )
+
 
 if __name__ == "__main__":
     uvicorn.run("boot:app", host="0.0.0.0", port=8000, reload=True)
