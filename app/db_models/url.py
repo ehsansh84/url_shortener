b710e4efc467a231e436f598fbeabbafc171d6bd
ehsan <ehsan.shirzadi@gmail.com>
2023-08-08 23:34:50 +0330

    added: url_is_correct removed: original_url

--- app/db_models/url.py
+++ app/db_models/url.py
@@ -10,10 +10,9 @@ class Url(DB):
     Use this model to manage a {module_text}
     """
 
-    def __init__(self, _id=None, module_name=module_name, module_text=module_text, db=None, user_id='', name=''):
+    def __init__(self, _id=None, module_name=module_name, module_text=module_text, db=None, url=''):
         self._id: str = _id
-        self.original_url: str = user_id
-        self.url: str = name
+        self.url: str = url
         super().__init__(_id=_id, module_name=module_name, module_text=module_text, db=db)
         self.order: int = self.new_order()
 
@@ -36,4 +35,5 @@ class Url(DB):
         else:
             return 1
 
-
+    def url_is_correct(self) -> bool:
+        return self.url.startswith("http://") or self.url.startswith("https://")
