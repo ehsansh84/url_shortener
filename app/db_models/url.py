5a96fc413bb82dbe8c609075551f4325aa1b713a
ehsan <ehsan.shirzadi@gmail.com>
2023-08-10 16:41:54 +0330

    added: visit and visited

--- app/db_models/url.py
+++ app/db_models/url.py
@@ -1,5 +1,7 @@
 from typing import List
 from app.core.base_db import DB
+from bson import ObjectId
+from datetime import datetime
 
 module_name = 'url'
 module_text = 'Url'
@@ -10,9 +12,10 @@ class Url(DB):
     Use this model to manage a {module_text}
     """
 
-    def __init__(self, _id=None, module_name=module_name, module_text=module_text, db=None, url=''):
+    def __init__(self, _id=None, module_name=module_name, module_text=module_text, db=None, url='', visits=0):
         self._id: str = _id
         self.url: str = url
+        self.visits: int = visits
         super().__init__(_id=_id, module_name=module_name, module_text=module_text, db=db)
         self.order: int = self.new_order()
 
@@ -37,3 +40,10 @@ class Url(DB):
 
     def url_is_correct(self) -> bool:
         return self.url.startswith("http://") or self.url.startswith("https://")
+
+    def visit(self) -> bool:
+        self.col.update_one({'_id': ObjectId(self._id)}, {
+            '$inc': {'visits': 1},
+            '$set': {'updated_at': datetime.now()}
+        })
+        return True
