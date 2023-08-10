9edc57314384af9929eb655ee4cd70bf09c64022
ehsan <ehsan.shirzadi@gmail.com>
2023-08-10 10:50:33 +0330

    updated: get url works fine

--- app/boot.py
+++ app/boot.py
@@ -2,7 +2,8 @@ import os
 import sys
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_dir)
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 import uvicorn
 from app.api.url import router as url_router
 from app.db_models.url import Url
@@ -11,22 +12,16 @@ app.include_router(router=url_router)
 
 
 @app.get("/{_id}", response_description=f"Redirect to original url")
-async def get_one(_id):
+async def get_one(_id, request: Request):
     from app.core.base62 import Base62
     if _id != 'favicon.ico':
         order = Base62.decode_base_62(_id)
         obj = Url()
         result = obj.list({'order': order})
-        print(result[0])
-        print(len(result))
-    #
-    # if obj.is_loaded():
-    #     return obj.to_json()
-    # else:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_404_NOT_FOUND,
-    #         detail=f'{module_text} with id {_id} not found!',
-    #     )
+        original_url = result[0].url
+        params = request.scope.get("query_string", b"").decode("utf-8")
+        final_url = f'{original_url}&{params}'
+        return RedirectResponse(url=final_url)
 
 
 if __name__ == "__main__":
